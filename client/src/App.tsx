dd1ca237789201374374129af9f74b9859d5011c
Quoc-Liem Vu <quocliemvu@gmail.com>
2023-04-27 16:05:03 -0700

    fixed minor duplicate code

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,7 +3,6 @@ import { ChakraProvider } from "@chakra-ui/react";
 import HangmanDrawing from "./components/HangmanDrawing";
 import Keyboard from "./components/Keyboard";
 import Hangman from "./pages/Hangman";
-import { ChakraProvider } from '@chakra-ui/react'
 import theme from "./theme.js";
 import Home from './pages/home.js'
 
